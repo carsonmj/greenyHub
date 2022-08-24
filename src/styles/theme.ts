1d15c404946d7ceb69fd7099f548835eb3cb37c0
eunoia2040 <eunoia2040@gmail.com>
2022-08-25 00:11:19 +0900

    feat: add initial theme style

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  red_1: "#F99898",
+  green_1: "#24CF73",
+  green_2: "#00E600",
+  green_3: "#BFF1D6",
+  gray_1: "#484747",
+  gray_2: "#5A5A5A",
+  lightgray_1: "#DCDCDC",
+  lightgray_2: "#ECECEC",
+};
+
+const theme = { colors };
+
+export default theme;
