6f7b2ce469d2eb5fbfd651780174deffbee3d0e7
eunoia2040 <eunoia2040@gmail.com>
2022-08-25 05:25:24 +0900

    feat: add Button Component

---
+++ src/common/components/Button.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+  width?: number;
+  height?: number;
+  radius?: number;
+  background?: string;
+  text: string;
+  onClick: () => void;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <Container {...props} onClick={props.onClick}>
+      {props.text}
+    </Container>
+  );
+};
+
+export default Button;
+
+const Container = styled.button<ButtonProps>`
+  width: ${({ width }) => (width ? width : "8rem")};
+  height: ${({ height }) => (height ? height : "3rem")};
+  background: ${({ theme, background }) => (background ? background : theme.colors.green_1)};
+  color: ${({ theme, color }) => (color ? color : theme.colors.white_1)};
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  border: 0;
+  border-radius: ${({ theme, radius }) => (radius ? radius : "4rem")};
+`;
