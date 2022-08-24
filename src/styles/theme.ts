220503307353184293b9ec65217507d2eea03f6a
eunoia2040 <eunoia2040@gmail.com>
2022-08-25 05:27:30 +0900

    style: add theme properties

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,29 @@
 const colors = {
   red_1: "#F99898",
-  green_1: "#24CF73",
-  green_2: "#00E600",
-  green_3: "#BFF1D6",
+  green_1: "#45c38a",
+  green_2: "#24CF73",
+  green_3: "#00E600",
+  green_4: "#BFF1D6",
+  green_5: "#edf9e0",
+  purple_1: "#e4e0f9",
   gray_1: "#484747",
   gray_2: "#5A5A5A",
   lightgray_1: "#DCDCDC",
   lightgray_2: "#ECECEC",
+  white_1: "#FFFFFF",
 };
 
-const theme = { colors };
+const fontSizes = {
+  xs: "0.75rem",
+  sm: "0.9rem",
+  base: "1rem",
+  lg: "1.2rem",
+  xl: "1.4rem",
+  xl2: "1.8rem",
+  xl3: "2rem",
+  xl4: "2.4rem",
+};
+
+const theme = { colors, fontSizes };
 
 export default theme;
