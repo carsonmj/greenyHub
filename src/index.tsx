fcef0252a50c19676ac7eb009e8a98fca455ce54
eunoia2040 <eunoia2040@gmail.com>
2022-08-25 00:11:55 +0900

    feat: add ThemeProvider, GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from "./App";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
